bd75cfa0e84239ed07abeb132aa3070512086522
kempe_1026183 <1026183>
2025-03-15 17:59:29 -0700

    Added to particle.h and quennel.cc

--- particle.h
+++ particle.h
@@ -14,3 +14,30 @@ class Cell {
 		void setPrev(Cell* newPrev);
 		void setParticle(particle newParticle);
 }
+
+class Particle {
+	double x;
+	double y;
+	double velocityX;
+	double velocityY;
+	double frames; //the lifetime of the particle
+	Movement movementType;
+
+	public:
+		Particle();
+		Particle(double new_x, double new_y, double new_velocityX, double new_velocityY, double new_frames, Movement new_movementType);
+		void set_x(double new_x);
+		void set_y(double new_y);
+		void set_velocityX(double new_velocityX);
+		void set_velocityY(double new_velocityY);
+		void set_frames(double new_frames);
+		void set_movementType(Movement new_movementType);
+		double get_x() const;
+		double get_y() const;
+		double get_velocityX() const;
+		double get_velocityY() const;
+		double get_frames() const;
+		void physics(Movement& movementType);
+		string draw(Movement& movementType);
+};
+
--- quennel.cc
+++ quennel.cc
@@ -1,4 +1,5 @@
-include "particle.h"
+#include "particle.h"
+#include <iostream>
 using namespace std;
 
 struct Movement {
@@ -8,67 +9,54 @@ struct Movement {
 	}
 };
 
-class Particle {
-	double x;
-	double y;
-	double velocityX;
-	double velocityY;
-	double frames; //the lifetime of the particle
-	Movement movementType;
-
-public: 
-	double
-	Particle() {
+Particle::Particle() {
 		x = 0;
 		y = 0;
 		velocityX = 0;
 		veolictyY = 0;
 		frames = 0;
-	}
-	Particle(double new_x, double new_y, double new_velocityX, double new_velocityY, double new_frames, Movement new_movementType) {
+}
+Particle::Particle(double new_x, double new_y, double new_velocityX, double new_velocityY, double new_frames, Movement new_movementType) {
 		x = new_x;
 		y = new_y;
 		velocityX = new_velocityX;
 		velocityY = new_velocityY;
 		frames = new_frames;
 		movementType = new_movementType;
-	}
-	void set_x(double new_x) {
+}
+void Particle::set_x(double new_x) {
 		x = new_x;
-	}
-	void set_y(double new_y) {
+}
+void Particle::set_y(double new_y) {
 		y = new_y;
-	}
-	void set_velocityX(double new_velocityX) {
+}
+void Particle::set_velocityX(double new_velocityX) {
 		velocityX = new_velocityX;
-	}
-	void set_velocityY(double new_velocityY) {
+}
+void Particle::set_velocityY(double new_velocityY) {
 		velocityY = new_velocityY;
-	}
-	void set_frames(double new_frames) {
+}
+void Particle::set_frames(double new_frames) {
 		frames = new_frames;
-	}
-	void set_movementType(Movement new_movementType) {
+}
+void Particle::set_movementType(Movement new_movementType) {
 		movementType = new_movementType;
-	}
-	double get_x() const { return x; }
+}
+double Particle::get_x() const { return x; }
 
-	double get_y() const { return y; }
+double Particle::get_y() const { return y; }
 
-	double get_velocityX() const { return velocityX; }
+double Particle::get_velocityX() const { return velocityX; }
 
-	double get_velocityY() const { return velocityY; }
+double Particle::get_velocityY() const { return velocityY; }
 
-	double get_frames() const { return frames; }
+double Particle::get_frames() const { return frames; }
 
-	void physics(Movement& movementType) {
+void Particle::physics(Movement& movementType) {
 		
-	}
+}
 
-	string draw(Movement& movementType) {
+string Particle::draw(Movement& movementType) {
 		return "stub";
-	}
+}
 
-
-
-};
