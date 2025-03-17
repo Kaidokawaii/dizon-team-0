2de338cf0e16f75880462b42813a51ed46c9f211
dizon_khai <dizon_khai@mail.fresnostate.edu>
2025-03-17 03:38:23 -0700

    added my test functions to the testing.cc file for particlesystem and particlegraphics

--- testing.cc
+++ testing.cc
@@ -3,6 +3,51 @@
 
 using namespace std;
 
+//Khai's test for Particle System Class
+    void testParticleSystem() {
+        ParticleSystem system(1080, 720);
+        cout << "Screen size: " << 1080 << x << 720 << "\n";
+
+        //Test add function
+        Particle* firstP = new Particle(100, 200);
+        Particle* secondP = new Particle(150, 250);
+        Particle* thirdP = new Particle(200, 300);
+
+        system.add(firstP);
+        system.add(secondP);
+        system.add(thirdP);
+
+        //Test numParticles function
+        cout << "Number of particles: " << system.numParticles() << "\n";
+
+        //Test moveParticles
+        system.moveParticles();
+
+        //Test drawParticles
+        system.drawParticles();
+
+        //Test drawWindow();
+        system.drawWindow();
+
+        //delete
+        delete firstP;
+        delete secondP;
+        delete thirdP;
+    }
+
+    void testParticleGraphics() {
+        ParticleGraphics graphics;
+        graphics.setColor("blue");
+        cout << "Current color: " << graphics.getColor() << "\n";
+
+        //Test drawing functions
+        graphics.drawPoint(50, 50);
+        graphics.drawRectangle(100, 100, 50, 25);
+        graphics.drawOval(200, 200, 25, 50);
+        graphics.drawPolygon({50, 70, 300, 70}); //fix for coords
+        graphics.drawLine{0, 0, 1080, 720);
+    }   
+
 int main() {
 	Movement m("FIREWORK");
 	Particle p1;
@@ -58,6 +103,10 @@ int main() {
 	cout << "Particle tt's velocity: (" << tt.get_velocityX() << "," << tt.get_velocityY() << ")" << endl;
 	cout << "Particle tt has " << tt.get_frames() << " frames." << endl;
 	cout << "Particle tt is " << tt.get_movementType() << endl;
+
+	//Khai testing functions:
+	testParticleSystem();
+	testParticleGraphics();
 
 	return 0;
 }
