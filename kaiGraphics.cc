d1cefae3accb6c13edc817f309d669d2be4d97a9
dizon_khai <dizon_khai@mail.fresnostate.edu>
2025-03-24 08:31:09 -0700

    Finally got colors to properly work with graphics

--- kaiGraphics.cc
+++ kaiGraphics.cc
@@ -3,68 +3,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
-/*
-//Usage:
-//Echo RED as part of a cout to turn the following text red
-//Echo RESET after you are done and it will go back to normal
-//For example: cout << RED << "Hello World" << RESET << endl;
-const std::string RESET   = "\033[0m";
-const std::string BLACK   = "\033[30m";      // Black 
-const std::string RED     = "\033[31m";      // Red 
-const std::string GREEN   = "\033[32m";      // Green
-const std::string YELLOW  = "\033[33m";      // Yellow
-const std::string BLUE    = "\033[34m";      // Blue 
-const std::string MAGENTA = "\033[35m";      // Magenta
-const std::string CYAN    = "\033[36m";      // Cyan 
-const std::string WHITE   = "\033[37m";      // White
-const std::string BOLDBLACK   = "\033[1m\033[30m";      // Bold Black
-const std::string BOLDRED     = "\033[1m\033[31m";      // Bold Red 
-const std::string BOLDGREEN   = "\033[1m\033[32m";      // Bold Green 
-const std::string BOLDYELLOW  = "\033[1m\033[33m";      // Bold Yellow 
-const std::string BOLDBLUE    = "\033[1m\033[34m";      // Bold Blue 
-const std::string BOLDMAGENTA = "\033[1m\033[35m";      // Bold Magenta 
-const std::string BOLDCYAN    = "\033[1m\033[36m";      // Bold Cyan 
-const std::string BOLDWHITE   = "\033[1m\033[37m";      // Bold White
-
-//Sets the background color for all text printed from this point on
-//Values range from 0 to 255 in each color channel
-void setbgcolor(uint8_t R, uint8_t G, uint8_t B) {
-    std::cerr << "\x1b[48;2;" << (int)R << ";" << (int)G << ";" << (int)B << "m";
-}
-
-//Sets the foreground color for all text printed from this point on
-void setcolor(uint8_t R, uint8_t G, uint8_t B) {
-    std::cerr << "\x1b[38;2;" << (int)R << ";" << (int)G << ";" << (int)B << "m";
-}
-
-//Sets the foreground and background colors back to the default
-void resetcolor() {
-    std::cerr << "\x1b[0m";
-}
-
-//These functions are like a mini NCURSES library
-
-//Returns the ROWS and COLS of the current terminal
-std::pair<int,int> get_terminal_size() {
-    struct winsize w;
-    ioctl(0, TIOCGWINSZ, &w);
-    return {w.ws_row-1, w.ws_col}; //Subtract 1 to give room for the UNIX prompt at the bottom of the screen
-}
-
-//Moves the cursor to the indicated row and column
-void movecursor(uint32_t row, uint32_t col) {
-    std::cerr << "\033[" << row << ";" << col << "H";
-}
-
-//Clears the screen
-void clearscreen() {
-    std::cerr << "\033[2J";
-}
-
-//copied from colors.h in the colors_demo directory instead of doing #include (dont need to go back and forth for colors)
-*/
 
 ParticleGraphics::ParticleGraphics() : R(255), G(255), B(255) {}
+
+string getAnsiColor(int r, int g, int b) {
+	if (r > 200 && g < 50 && b < 50) return RED;
+    if (g > 200 && r < 50 && b < 50) return GREEN;
+    if (b > 200 && r < 50 && g < 50) return BLUE;
+    if (r > 200 && g > 200 && b < 50) return YELLOW;
+    if (r > 200 && b > 200 && g < 50) return MAGENTA;
+    if (g > 200 && b > 200 && r < 50) return CYAN;
+    if (r > 200 && g > 200 && b > 200) return WHITE;
+    return WHITE; // default
+}
 
 int ParticleGraphics::getColorR() {
 	return R;
@@ -77,16 +28,23 @@ int ParticleGraphics::getColorB() {
 }
 
 void ParticleGraphics::setColor(int newR, int newG, int newB) {
+	::setcolor(newR, newG, newB);
+	
 	R = newR;
 	G = newG;
 	B = newB;
 }
 
 void ParticleGraphics::drawPoint(double x, double y) {
-	cerr << "\x1b[38;2;" << R << ";" << G << ";" << B << "m";  // Set RGB color
-    cerr << "\x1b[" << (int)y+1 << ";" << (int)x+1 << "H";     // Move cursor
-    cerr << "■";                                               // Block character
-    cerr << "\x1b[0m";                                         // Reset color
+    if (x<0 || y < 0) return;
+
+	string color = getAnsiColor(R,G,B);
+
+	cerr << color
+		 << "\033[" << (int)y+1 << ";" << (int)x+1 << "H"
+		 << "█" 
+		 << RESET;
+
     cerr.flush();
 }
 
