01ebef822a4e00be875212f418019672df50fe00
Quennel K <qkem08@yahoo.com>
2025-03-26 13:22:16 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,9 +1,6 @@
 #include "particle.h"
-<<<<<<< HEAD
 #include "quennel_screenSaver.h"
-=======
 #include "ethanProjec.h"
->>>>>>> 6f6ac9f269e2eb6eabb7a03caee75391290c7050
 #include <cassert>
 
 using namespace std;
